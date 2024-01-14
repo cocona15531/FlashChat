b7c019fc7776e4fe12976ec2d2cfd8e9a6c68a0b
issei.ueda <ron.ron633662@gmail.com>
2024-01-14 15:55:42 +0900

    tableviewcellに文字を表示

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Issei Ueda on 2024/01/14.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String //メールアドレス
+    let body: String //メッセージ本文
+}
