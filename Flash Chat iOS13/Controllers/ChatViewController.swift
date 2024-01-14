12a87a455b87f21e3f2d8f4f7a60a793a85a7013
issei.ueda <ron.ron633662@gmail.com>
2024-01-15 01:59:46 +0900

    データを時間の昇順に並べた

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -33,7 +33,9 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
         //getDocumentsは一度だけデータを取得したいときに使い、addSnapshotListenerはリアルタイムでデータを取得したいときに使う
-        db.collection(K.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
             self.messages = []
             if let e = error {
                 print("firestoreからのデータ取得に問題がありました, \(e)")
@@ -58,7 +60,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970 //1970年からの経過時間
             ]) { error in
                 if let e = error {
                     print("There was an issue saving data to firestore, \(e)")
